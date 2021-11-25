af802dd531607e3f4f44279b8564fc5d6b02fc33
lumos-ap <ankita008paul@gmail.com>
2021-11-25 23:06:10 +0530

    Temperature conversion created

---
+++ src/main/java/calculator/operations/conversion/Temperature.java
@@ -0,0 +1,62 @@
+package calculator.operations.conversion;
+
+import java.util.Scanner;
+
+public class Temperature {
+    double temp;
+    String fromUnit;
+    String toUnit;
+
+    public Temperature() {
+    }
+
+    public void run() {
+        getInput();
+        double result = convertTemperature(toUnit, fromUnit, temp);
+        System.out.println("Your answer is " + result + fromUnit );
+    }
+
+    private void getInput() {
+        Scanner scannerObject = new Scanner(System.in);
+        System.out.println("Enter Original Temperature:");
+        temp = scannerObject. nextDouble();
+
+        Scanner UserInput = new Scanner(System.in);
+        System.out.println("Enter your current unit (F/C/K):");
+        fromUnit = UserInput.next();
+        System.out.println("Enter unit to convert to (F/C/K):");
+        toUnit = UserInput.next();
+    }
+
+
+    public double  convertTemperature(String toUnit, String fromUnit, double temp) {
+
+        if(toUnit.equals(fromUnit))
+            return temp;
+        if(toUnit.toLowerCase().equals("k") && fromUnit.toLowerCase().equals("c") )
+        {
+            return (temp + 273.15);
+        }
+        if(toUnit.toLowerCase().equals("c") && fromUnit.toLowerCase().equals("k") )
+        {
+            return (temp - 273.15);
+        }
+        if(toUnit.toLowerCase().equals("k") && fromUnit.toLowerCase().equals("f") )
+        {
+            return ( 273.5 + ((temp - 32.0) * (5.0/9.0)));
+        }
+        if(toUnit.toLowerCase().equals("f") && fromUnit.toLowerCase().equals("k") )
+        {
+            return  ((9.0 / 5) * (temp - 273.15) + 32);
+        }
+        if(toUnit.toLowerCase().equals("c") && fromUnit.toLowerCase().equals("f") )
+        {
+            return   (((temp - 32) * 5) / 9.0);
+        }
+        if(toUnit.toLowerCase().equals("f") && fromUnit.toLowerCase().equals("c") )
+        {
+            return ((temp * 9.0 / 5.0) + 32.0);
+        }
+        throw new IllegalStateException("Math toUnit is not valid");
+    }
+}
---
+++ src/test/java/calculator/operations/conversion/TemperatureTest.java
@@ -0,0 +1,26 @@
+package calculator.operations.conversion;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void convertTemperature() {
+    }
+}
